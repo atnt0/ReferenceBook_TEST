6ef0e1cac199dee7ea6371de70936ea42ede42ad
vasyaxhd5@gmail.com <vasyaxhd5@gmail.com>
2020-11-05 00:07:56 +0200

    Добавлен      IGenericRepository

---
+++ BR.DAL/Common/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BR.DAL.Common
+{
+    public interface IGenericRepository<T, TKey> where T : class, new()
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> FindBy(Expression<Func<T, bool>> predicate);
+        T Get(TKey id);//
+        void Create(T obj);
+        void Update(T obj);
+        void Delete(TKey id);
+        void Save();
+
+    }
+}
