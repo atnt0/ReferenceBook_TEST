b09c53f61ebbcc9ca12a1a6a9f5d2af058f82755
vasyaxhd5@gmail.com <vasyaxhd5@gmail.com>
2020-11-05 01:27:41 +0200

    добавлен PhonesRepository

---
+++ BR.DAL/Repositories/PhonesRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RB.DAL.Common;
+using RB.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RB.DAL.Repositories
+{
+    public class PhonesRepository : GenericRepository<Phones, Guid>
+    {
+        public PhonesRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
