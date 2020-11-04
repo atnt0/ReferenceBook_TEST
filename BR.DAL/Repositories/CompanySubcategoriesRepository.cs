5021983e2e56fab14a4b6a7ddc5fff643408cf16
vasyaxhd5@gmail.com <vasyaxhd5@gmail.com>
2020-11-05 01:19:35 +0200

    добавлен   CompanySubcategoriesRepository

---
+++ BR.DAL/Repositories/CompanySubcategoriesRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RB.DAL.Common;
+using RB.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RB.DAL.Repositories
+{
+    class CompanySubcategoriesRepository : GenericRepository<CompanySubcategories, Guid>
+    {
+        public CompanySubcategoriesRepository(DbContext context) : base(context)
+        {
+        }
+        public override IQueryable<CompanySubcategories> GetAll()
+        {
+            return dbSet.Include(a => a.Subcategory);
+
+        }
+    }
+}
